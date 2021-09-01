efd500c15540e97156d484c3427d032e16a4ccd0
Aya Mosaad Mohamed <36517014+AyaMosaad1@users.noreply.github.com>
2021-09-01 09:58:20 +0200

    Init of the  project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+      <header >
+        <Button variant="primary">Secondary</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
